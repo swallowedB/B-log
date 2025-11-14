1504f57ceaf957a3072a75f618c11ba5e6df3296
choiboa <musamea99@gmail.com>
2025-11-14 11:38:50 +0900

    ✨ feat: Header/Footer 기본 레이아웃 구조 구현

--- app/(shell)/_components/layout/SiteFooter.tsx
+++ app/(shell)/_components/layout/SiteFooter.tsx
@@ -1,6 +1,17 @@
+import Link from "next/link";
+import { FaGithub } from "react-icons/fa";
 
 export default function SiteFooter() {
   return (
-    <div>SiteFooter</div>
-  )
+    <footer className="flex gap-5 items-center">
+      <Link href="/" aria-label="홈으로 이동">
+        Logo
+      </Link>
+      <p>© {new Date().getFullYear()} CHOI BOA · All Rights Reserved</p>
+      <a href="https://github.com/swallowedB" target="_blank" rel="noreferrer" className="flex gap-2 items-center">
+        <FaGithub />
+        <span>@swallowedB</span>
+      </a>
+    </footer>
+  );
 }
--- app/(shell)/_components/layout/SiteHeader.tsx
+++ app/(shell)/_components/layout/SiteHeader.tsx
@@ -1,6 +1,33 @@
+"use client";
+
+import Link from "next/link";
 
 export default function SiteHeader() {
   return (
-    <div>SiteHeader</div>
-  )
+    <header className="flex items-center justify-between ">
+      <div className="flex gap-8">
+        <Link href="/" aria-label="홈으로 이동">
+          B-log
+        </Link>
+
+        <nav aria-label="주요 페이지">
+          <ul className="flex gap-5">
+            <li>
+              <Link href="/resume">Resume</Link>
+            </li>
+            <li>
+              <Link href="/guestbook">Guestbook</Link>
+            </li>
+            <li>
+              <Link href="/lab">Lab</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <button type="button" aria-label="테마 변경" className="cursor-pointer">
+        테마
+      </button>
+    </header>
+  );
 }
